eab490cfb6994a0218600c301cf775198004a7ce
samuhlo <samu13lop@gmail.com>
2026-01-14 09:14:53 +0100

    frist commit : setting up project + navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { navLinks } from "../constants";
+</script>
+
+<template>
+  
+<header>
+            <nav>
+                <img  src="/logo.svg" alt="Apple logo" />
+
+                <ul>
+                  
+                    <li v-for="link in navLinks" :key="link.label">
+                      <a :href="link.label">{{ link.label }}</a>
+                    </li>
+                </ul>
+
+                <div className="flex-center gap-3">
+                    <button>
+                        <img src="/search.svg" alt="Search" />
+                    </button>
+                    <button>
+                        <img src="/cart.svg" alt="Cart" />
+                    </button>
+                </div>
+            </nav>
+        </header>
+  
+</template>
+
+
